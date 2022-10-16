c65cdae0d574035531f3821da57c38445b07f275
Mihail Petrakov <mihailpetrakov065@gmail.com>
2022-10-16 19:55:53 +0300

    Update IndexController.php

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -31,16 +31,15 @@ class IndexController extends Controller
         $callbackQuery = $webhookData->callbackQuery;
 
         file_put_contents(__DIR__ . '/../../message.txt', print_r($webhookData, true) . "\n", FILE_APPEND | LOCK_EX);
-        file_put_contents(__DIR__ . '/../../message.txt', print_r($callbackQuery, true) . "\n", FILE_APPEND | LOCK_EX);
 
-        /*
-        if ($callbackQuery->isNotEmpty()) {
-            $callbackData = collect(explode('-', $callbackQuery->get('data')));
-            file_put_contents(__DIR__ . '/../../message.txt', print_r($callbackData, true) . "\n", FILE_APPEND | LOCK_EX);
-            if ($callbackData->isNotEmpty() && $callbackData[0] === 'calendar') {
-                (new CalendarAction($callbackData))->handle();
-            }
+        if (is_null($callbackQuery)) {
+            $this->success(['message' => 'Empty callbackQuery']);
         }
-        */
+
+        $callbackData = collect(explode('-', $callbackQuery->get('data')));
+        file_put_contents(__DIR__ . '/../../message.txt', print_r($callbackData, true) . "\n", FILE_APPEND | LOCK_EX);
+//        if ($callbackData->isNotEmpty() && $callbackData[0] === 'calendar') {
+//            (new CalendarAction($callbackData))->handle();
+//        }
     }
 }
