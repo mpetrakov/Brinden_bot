d34cbbfe07f2804ad1af031e1ad2562747b9a11f
Mihail Petrakov <mihailpetrakov065@gmail.com>
2022-10-16 17:50:06 +0300

    test commands v3

--- src/Commands/StartCommand.php
+++ src/Commands/StartCommand.php
@@ -9,18 +9,14 @@ class StartCommand extends Command
 {
     protected $name = 'start';
 
-    protected $description = 'Start Command to get you started';
+    protected $description = 'Команда /start';
 
     public function handle()
     {
-        // This will send a message using `sendMessage` method behind the scenes to
-        // the user/chat id who triggered this command.
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
+        $this->replyWithMessage(['text' => 'Привет, ковбой!']);
 
         // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
 
         // This will prepare a list of available commands and send the user.
         // First, Get an array of all registered commands
